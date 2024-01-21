daee88ce79795971bd7d8addb7fbdac4c56f644d
Reuven <ramatiry@gmail.com>
2024-01-21 17:13:15 +0200

    nest initialization

---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Observable, throwError } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AxiosResponse } from 'axios';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly httpService: HttpService) {}
+
+  private readonly externalApiUrl =
+    'https://jsonplaceholder.typicode.com/users';
+
+  fetchUsers(): Observable<AxiosResponse<any[]>> {
+    return this.httpService.get<any[]>(this.externalApiUrl).pipe(
+      catchError((error) => {
+        console.error('Error fetching users:', error);
+        return throwError(() => error);
+      }),
+    );
+  }
+
+  async getUsers(): Promise<any[]> {
+    try {
+      const response = await lastValueFrom(this.fetchUsers());
+      return response.data;
+    } catch (error) {
+      console.error('Error in getUsers:', error);
+      throw new Error('Failed to fetch users');
+    }
+  }
+}
